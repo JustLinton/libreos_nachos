6467b3a89e86e304711ca0eec0ae80c9ae03f35e
JustLinton <530065614@qq.com>
2022-04-25 17:01:39 +0800

    auto commit

--- main.cc
+++ main.cc
@@ -104,6 +104,7 @@ main(int argc, char **argv)
             printf (copyright);
 #ifdef USER_PROGRAM
 	
+		printf("%s, %d",*argv,strcmp(*argv, "-x"));
 
 		if (!strcmp(*argv, "-x")) {        	// run a user program
 			ASSERT(argc > 1);
