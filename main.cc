2776c698abb6b835facf2c2a228ade5ae55e5133
JustLinton <530065614@qq.com>
2022-04-25 09:43:24 +0800

    auto commit

--- main.cc
+++ main.cc
@@ -105,20 +105,20 @@ main(int argc, char **argv)
 		StartProcess(*(argv + 1));
 		argCount = 2;
 			
-		} else if (!strcmp(*argv, "-c")) {
-			printf("prt!\n"); // test the console
-			if (argc == 1)
-				ConsoleTest(NULL, NULL);
-			else
-			{
-				ASSERT(argc > 2);
-				ConsoleTest(*(argv + 1), *(argv + 2));
-				argCount = 3;
+		} else if (!strcmp(*argv, "-c")) {    
+			  // test the console
+	    if (argc == 1)
+	        ConsoleTest(NULL, NULL);
+	    else {
+		ASSERT(argc > 2);
+	        ConsoleTest(*(argv + 1), *(argv + 2));
+	        argCount = 3;
 	    }
 	    interrupt->Halt();		// once we start the console, then 
 					// Nachos will loop forever waiting 
 					// for console input
 	}
+	printf("prt!\n");
 #endif // USER_PROGRAM
 #ifdef FILESYS
 	if (!strcmp(*argv, "-cp")) { 		// copy from UNIX to Nachos
