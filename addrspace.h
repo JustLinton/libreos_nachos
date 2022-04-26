c04012aae3a57e85998f71b936d0eed1633121fc
JustLinton <530065614@qq.com>
2022-04-26 20:11:39 +0800

    auto commit

--- addrspace.h
+++ addrspace.h
@@ -41,14 +41,13 @@ class AddrSpace
     void releaseFileDescriptor(int fd);
   // #endif
 
-    unsigned int numPages; // 页表中的页表项
   private:
     // add *userMap, *pidMap,spaceId
     static BitMap *userMap, *pidMap; // 全局位图
     TranslationEntry *pageTable;     // 线性页表
     unsigned int spaceId;
-
-   
+    unsigned int numPages;  // 页表中的页表项
+    unsigned int numPages_2;  // 页表中的页表项
 };
 
 #endif // ADDRSPACE_H
