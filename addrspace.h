4bc7dce74e9ba4d7da33cd161b40b8cf89b28772
JustLinton <530065614@qq.com>
2022-04-26 20:09:24 +0800

    auto commit

--- addrspace.h
+++ addrspace.h
@@ -45,8 +45,9 @@ class AddrSpace
     // add *userMap, *pidMap,spaceId
     static BitMap *userMap, *pidMap; // 全局位图
     TranslationEntry *pageTable;     // 线性页表
-    unsigned int numPages;  // 页表中的页表项以及地址编号
     unsigned int spaceId;
+    unsigned int numPages;  // 页表中的页表项
+   
 };
 
 #endif // ADDRSPACE_H
