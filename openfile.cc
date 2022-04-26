d0eab57e749458cdb37ff7cc850b89dcfd91a799
JustLinton <530065614@qq.com>
2022-04-26 11:42:18 +0800

    auto commit

--- openfile.cc
+++ openfile.cc
@@ -147,8 +147,11 @@ OpenFile::ReadAt(char *into, int numBytes, int position)
     int i, firstSector, lastSector, numSectors;
     char *buf;
 
-    if ((numBytes <= 0) || (position >= fileLength))
-    	return 0; 				// check request
+    if ((numBytes <= 0) || (position >= fileLength)){
+        return 0; // check request
+        printf("invalid read!\n");
+    }
+    	
     if ((position + numBytes) > fileLength)		
 	numBytes = fileLength - position;
     DEBUG('f', "Reading %d bytes at %d, from file of length %d.\n", 	
